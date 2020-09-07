44fb8028507bc51503516d48b6d00e5f40d5391b
Oleksandr Nazarenko <bahures@ukr.net>
2020-09-07 12:26:09 +0300

    first commit

---
+++ js/task-02.js
@@ -0,0 +1,11 @@
+"use strict";
+const countProps = function (obj) {
+  const items = Object.keys(obj);
+  return items.length;
+};
+
+console.log(countProps({})); // 0
+
+console.log(countProps({ name: "Mango", age: 2 })); // 2
+
+console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 })); // 3
